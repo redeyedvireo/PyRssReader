399d0b4412c19a027feac7ca33fbaa9496141555
Jeff <abc@abc.com>
2017-02-18 10:36:45 -0500

    Add keyboard support for navigating feed items.

--- title_tree.py
+++ title_tree.py
@@ -1,5 +1,5 @@
 import logging
-from PyQt5 import QtCore, QtGui
+from PyQt5 import QtCore, QtGui, QtWidgets
 from language_filter import LanguageFilter
 from title_tree_view_item import TitleTreeViewItem, kEnclosureColumn, kTitleColumn, kDateColumn, kCreatorColumn, kTagsColumn, kNumColumns
 from title_tree_date_item import TitleTreeDateItem
@@ -11,6 +11,7 @@ kEnclosureColumnWidth = 40
 
 class TitleTree(QtCore.QObject):
     feedItemSelectedSignal = QtCore.pyqtSignal(str)
+    movementKeys = [ QtCore.Qt.Key_Up, QtCore.Qt.Key_Down, QtCore.Qt.Key_PageUp, QtCore.Qt.Key_PageDown ]
 
     def __init__(self, treeView, languageFilter):
         super(TitleTree, self).__init__()
@@ -21,8 +22,9 @@ class TitleTree(QtCore.QObject):
         self.sortOrder = QtCore.Qt.DescendingOrder
         self.configureTree()
         self.titleTreeView.header().sortIndicatorChanged.connect(self.onSortIndicatorChanged)
-        self.titleTreeView.clicked.connect(self.onItemChanged)
+        self.enableUserActions()
         self.m_Grouper = None
+        self.titleTreeView.installEventFilter(self)
 
     def configureTree(self):
         self.model = QtGui.QStandardItemModel()
@@ -33,10 +35,34 @@ class TitleTree(QtCore.QObject):
         self.titleTreeView.sortByColumn(self.sortColumn, self.sortOrder)
         self.titleTreeView.setUniformRowHeights(True)
 
+    def enableUserActions(self):
+        """ Enables the tree to respond to user actions. """
+        self.titleTreeView.clicked.connect(self.onItemChanged)
+        self.titleTreeView.activated.connect(self.onItemChanged)
+
+    def disableUserActions(self):
+        """ Disables the tree to respond to user actions. """
+        self.titleTreeView.clicked.disconnect()
+        self.titleTreeView.activated.disconnect()
+
+    def eventFilter(self, obj, event):
+        if obj == self.titleTreeView:
+            if event.type() == QtCore.QEvent.KeyRelease:
+                print("key: {}".format(event.key()))
+                self.handleKeyRelease(event.key())
+                return False
+
+        return QtWidgets.QTreeView.eventFilter(self.titleTreeView, obj, event)
+
+    def handleKeyRelease(self, keyName):
+        if keyName in self.movementKeys:
+            # Qt takes care of moving the selected item.  All we need to do is update the content view.
+            self.onItemChanged(self.titleTreeView.currentIndex())
+
     def onItemChanged(self, index):
         item = self.model.item(index.row(), kTitleColumn)
         feedItemGuid = item.guid()
-        print("Row clicked: {}, GUID: {}".format(index.row(), feedItemGuid))
+        print("Row clicked: {}, GUID: {}".format(item.row(), feedItemGuid))
         self.feedItemSelectedSignal.emit(feedItemGuid)
 
     def onSortIndicatorChanged(self, logicalIndex, order):
@@ -85,7 +111,7 @@ class TitleTree(QtCore.QObject):
             item.setSizeHint(sizeHint)
 
     def addFeedItems(self, feedItemList):
-        self.titleTreeView.clicked.disconnect()
+        self.disableUserActions()
 
         numRows = self.model.rowCount()
         if numRows > 0:
@@ -97,7 +123,7 @@ class TitleTree(QtCore.QObject):
         self.titleTreeView.sortByColumn(self.sortColumn, self.sortOrder)
         self.model.sort(self.sortColumn, self.sortOrder)
 
-        self.titleTreeView.clicked.connect(self.onItemChanged)
+        self.enableUserActions()
 
     def GetColumnWidths(self):
         """ Returns the widths of all columns """
