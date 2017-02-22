9f4928af298dd1b6a7b0c04eb94d4fbdee60495e
Jeff <abc@abc.com>
2017-02-21 19:30:26 -0500

    Slight cleanup of resource fetcher.

--- resource_fetcher.py
+++ resource_fetcher.py
@@ -10,11 +10,9 @@ class ResourceFetcher(object):
 
         try:
             if self.proxy.usesProxy():
-                proxyAndPortStr = 'http://{}:{}@{}:{}'.format(self.proxy.proxyUser, self.proxy.proxyPassword, self.proxy.proxyUrl, self.proxy.proxyPort)
                 proxy_handler = ProxyHandler({'http': 'http://{}:{}@{}:{}'.format(self.proxy.proxyUser, self.proxy.proxyPassword, self.proxy.proxyUrl, self.proxy.proxyPort),
                                               'https': 'https://{}:{}@{}:{}'.format(self.proxy.proxyUser, self.proxy.proxyPassword, self.proxy.proxyUrl, self.proxy.proxyPort)})
                 proxy_auth_handler = HTTPBasicAuthHandler()
-                #proxy_auth_handler.add_password('realm', self.proxy.proxyUrl, self.proxy.proxyUser, self.proxy.proxyPassword)
                 opener = request.build_opener(proxy_handler, proxy_auth_handler, HTTPHandler)
 
                 # This installs it globally, so it can be used with urlopen().
@@ -22,30 +20,25 @@ class ResourceFetcher(object):
 
             self.request = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
             self.data = urlopen(self.request).read()
-        except HTTPError as e:
-            errMsg = "HTTP error fetching: {}: {}".format(url, e.code)
-            print(errMsg)
-            logging.error(errMsg)
 
-            # Set a NULL image to data
-            nullImage = QtGui.QImage()
-            byteArray = QtCore.QByteArray()
-            buffer =  QtCore.QBuffer(byteArray)
-            buffer.open(QtCore.QIODevice.WriteOnly)
-            nullImage.save(buffer, "PNG")
-            self.data = byteArray
         except URLError as e:
-            errMsg = "URL Error fetching: {}: {}".format(url, e.reason)
+            if hasattr(e, 'reason'):
+                errMsg = "Could not connect to server when fetching: {}: {}".format(url, e.reason)
+            elif hasattr(e, 'code'):
+                errMsg = "Could not fulfill request when fetching: {}: {}".format(url, e.code)
+
             print(errMsg)
             logging.error(errMsg)
 
-            # Set a NULL image to data
-            nullImage = QtGui.QImage()
-            byteArray = QtCore.QByteArray()
-            buffer =  QtCore.QBuffer(byteArray)
-            buffer.open(QtCore.QIODevice.WriteOnly)
-            nullImage.save(buffer, "PNG")
-            self.data = byteArray
+            self.data = self.createNullImage()
 
     def getData(self):
         return self.data
+
+    def createNullImage(self):
+        nullImage = QtGui.QImage()
+        byteArray = QtCore.QByteArray()
+        buffer = QtCore.QBuffer(byteArray)
+        buffer.open(QtCore.QIODevice.WriteOnly)
+        nullImage.save(buffer, "PNG")
+        return byteArray
