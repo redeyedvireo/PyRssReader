acac098560de90beffc7e5f914834fbd26217fdc
Jeff <abc@abc.com>
2017-04-05 21:26:34 -0400

    Show minutes in pub date in title tree so that feed items will sort by time as well as by date.

--- title_tree_date_item.py
+++ title_tree_date_item.py
@@ -3,5 +3,5 @@ from title_tree_view_item import TitleTreeViewItem
 
 class TitleTreeDateItem(TitleTreeViewItem):
     def __init__(self, date, bRead, feedId, guid):
-        dateText = "{}".format(date.strftime("%Y-%m-%d"))
+        dateText = "{}".format(date.strftime("%Y-%m-%d %H:%M:%S"))
         TitleTreeViewItem.__init__(self, dateText, bRead, feedId, guid)
