edc911c6f1537e8f2d77cd53f9c94c3cc08f6f83
Jeff <abc@abc.com>
2022-04-23 12:01:32 -0400

    Add function to set the proxy dictionary.

--- proxy.py
+++ proxy.py
@@ -7,13 +7,16 @@ class Proxy:
         self.proxyUser = ""
         self.proxyPassword = ""
 
-        self.proxyDict = { 'http': 'http://{}:{}@{}:{}/'.format(self.proxy.proxyUser, self.proxy.proxyPassword,
-                                                            self.proxy.proxyUrl, self.proxy.proxyPort),
-                            'https': 'https://{}:{}@{}:{}/'.format(self.proxy.proxyUser, self.proxy.proxyPassword,
-                                                            self.proxy.proxyUrl, self.proxy.proxyPort) }
+        self.setProxyDict()
 
     def __str__(self):
         return "Host: {}, Port: {}, User: {}, Password: {}".format(self.proxyUrl, self.proxyPort, self.proxyUser, self.proxyPassword)
 
     def usesProxy(self):
         return len(self.proxyUrl) > 0
+
+    def setProxyDict(self):
+        self.proxyDict = { 'http': 'http://{}:{}@{}:{}/'.format(self.proxyUser, self.proxyPassword,
+                                                    self.proxyUrl, self.proxyPort),
+                            'https': 'https://{}:{}@{}:{}/'.format(self.proxyUser, self.proxyPassword,
+                                                            self.proxyUrl, self.proxyPort) }
