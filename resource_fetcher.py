dc6d57e8f4d8808fadf52d6f643433004843d1be
Jeff <abc@abc.com>
2024-02-17 10:55:48 -0500

    Add additional stuff to the User Agent header, to prevent some feed servers from ignoring the feed fetch request.

--- resource_fetcher.py
+++ resource_fetcher.py
@@ -17,7 +17,7 @@ class ResourceFetcher:
                 # This installs it globally, so it can be used with urlopen().
                 request.install_opener(opener)
 
-            self.request = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+            self.request = Request(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0'})
             self.data = urlopen(self.request).read()
 
         except URLError as e:
